c4341ad21243c3ebd052290f893c8c2b15ad5e4e
Trahan.hunter <Trahan.hunter@yahoo.com>
2019-09-09 16:29:36 -0500

    added multple forms of sortting

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -15,37 +15,112 @@ namespace BubbleSort
         static void Main(string[] args)
         {
             //int[] arrayToSort = { 45, 6, 12, 51, 4, 23, 31 };
+
             int[] arrayToSort = { 14, 65, 63, 1, 54, 89, 84, 9, 98, 57,
                                   71, 18, 21, 84, 69, 28, 11, 83, 13, 42,
                                   64, 58, 78, 82, 13, 9, 96, 14, 39, 89,
                                   40, 32, 51, 85, 48, 40, 23, 15, 94, 93,
-                                  35, 81, 1, 9, 43, 39, 15, 17, 97, 52};
-           
-            //this is declaration
-            
+                                  35, 81, 1, 9, 43, 39, 15, 17, 97, 52
+            };
 
-            
+
+           //this is declaration
            void Print()
            {
-                for (int i = 0; i < arrayToSort.Length; i++)
-                {
-                    Console.WriteLine(arrayToSort[i]);
-                }
+               for (int i = 0; i < arrayToSort.Length; i++)
+               {
+                   Console.WriteLine(arrayToSort[i]);
+               }
            }
 
-            BubbleSort(arrayToSort);
+            //BubbleSort(arrayToSort);
+            Console.WriteLine("");
+            //OptimizedBubbleSort(arrayToSort);
+            Console.WriteLine("");
+            CocktailShakerSort(arrayToSort);
             Print();
-
 
             Console.WriteLine("");
             Console.WriteLine("Loops: " + loops);
             Console.WriteLine("Swaps: " + swaps);
             Console.ReadKey();
-
         }
 
-        public static void BubbleSort(int[] arrayToSort)
+        //public static void BubbleSort(int[] arrayToSort)
+        //{
+
+        //    int number;
+
+        //    //nark that we need to sort
+        //    bool Sorted = false;
+        //    //while we need to sort
+        //    while (!Sorted)
+        //    {
+        //        Sorted = true;
+        //        //Iterate through array
+        //        for (int i = 0; i < arrayToSort.Length - 1; i++)
+        //        {
+        //            //if current item and the next aren't in order
+        //            if (arrayToSort[i] > arrayToSort[i + 1])
+        //            {
+        //                //swap them
+        //                number = arrayToSort[i];
+        //                arrayToSort[i] = arrayToSort[i + 1];
+        //                arrayToSort[i + 1] = number;
+
+        //                //mark that we need to sort
+        //                Sorted = false;
+        //                swaps++;
+        //                //i = 0;
+
+        //            }
+        //            loops++;
+
+        //        }
+
+        //    }
+        //}
+
+
+        //public static void OptimizedBubbleSort(int[] arrayToSort)
+        //{
+        //    int number1 = 1;
+        //    int number;
+
+        //    //nark that we need to sort
+        //    bool Sorted = false;
+        //    //while we need to sort
+        //    while (!Sorted)
+        //    {
+        //        Sorted = true;
+        //        //Iterate through array
+        //        for (int i = 0; i < arrayToSort.Length - number1; i++)
+        //        {
+        //            //if current item and the next aren't in order
+        //            if (arrayToSort[i] > arrayToSort[i + 1])
+        //            {
+        //                //swap them
+        //                number = arrayToSort[i];
+        //                arrayToSort[i] = arrayToSort[i + 1];
+        //                arrayToSort[i + 1] = number;
+
+        //                //mark that we need to sort
+        //                Sorted = false;
+        //                swaps++;
+        //                //i = 0;
+
+        //            }
+                    
+        //            loops++;
+        //        }
+        //        number1++;
+        //    }
+        //}
+
+        public static void CocktailShakerSort(int[] arrayToSort)
         {
+            int start = 0;
+            int end = arrayToSort.Length - 1;
             int number;
 
             //nark that we need to sort
@@ -55,9 +130,9 @@ namespace BubbleSort
             {
                 Sorted = true;
                 //Iterate through array
-                for (int i = 0; i < arrayToSort.Length - 1; i++)
+                for (int i = start; i < end; i++)
                 {
-                    //if current item and the next arem't in order
+                    //if current item and the next aren't in order
                     if (arrayToSort[i] > arrayToSort[i + 1])
                     {
                         //swap them
@@ -71,11 +146,34 @@ namespace BubbleSort
                         //i = 0;
 
                     }
+
                     loops++;
-
                 }
+                end--;
+                
 
+                for (int i = end; i > start; i--)
+                {
+                    //if current item and the next aren't in order
+                    if (arrayToSort[i] < arrayToSort[i - 1])
+                    {
+                        //swap them
+                        number = arrayToSort[i];
+                        arrayToSort[i] = arrayToSort[i - 1];
+                        arrayToSort[i - 1] = number;
+
+                        //mark that we need to sort
+                        Sorted = false;
+                        swaps++;
+                        //i = 0;
+
+                    }
+
+                    loops++;
+                }
+                start++;
             }
         }
+
     }
 }
